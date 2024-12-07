788af21285f29d9fcc255fd4c29e328a5c5b1f7b
KauanHerbst <kauanherbst7@gmail.com>
2024-12-07 14:27:53 -0300

    feat: button back to top

--- src/js/index.js
+++ src/js/index.js
@@ -1,34 +1,47 @@
 const sections = [
-    { id: "about-me", linkId: "link-about-me" },
-    { id: "skills", linkId: "link-skills" },
-    { id: "academic", linkId: "link-academic" },
-    { id: "experience", linkId: "link-experience" },
-    { id: "projects", linkId: "link-projects" }
-  ];
-  
-  window.addEventListener("scroll", () => {
-    let currentSection = "";
+  { id: "about-me", linkId: "link-about-me" },
+  { id: "skills", linkId: "link-skills" },
+  { id: "academic", linkId: "link-academic" },
+  { id: "experience", linkId: "link-experience" },
+  { id: "projects", linkId: "link-projects" },
+];
 
-    sections.forEach((section) => {
-      const sectionElement = document.getElementById(section.id);
-      if (sectionElement) {
-        const sectionTop = sectionElement.offsetTop;
-        const sectionHeight = sectionElement.offsetHeight;
-  
-        if (window.scrollY >= sectionTop - sectionHeight / 3) {
-          currentSection = section.id;
-        }
+window.addEventListener("scroll", () => {
+  let currentSection = "";
+
+  sections.forEach((section) => {
+    const sectionElement = document.getElementById(section.id);
+    if (sectionElement) {
+      const sectionTop = sectionElement.offsetTop;
+      const sectionHeight = sectionElement.offsetHeight;
+
+      if (window.scrollY >= sectionTop - sectionHeight / 3) {
+        currentSection = section.id;
       }
-    });
-  
-    sections.forEach((section) => {
-      const linkElement = document.getElementById(section.linkId);
-      if (linkElement) {
-        if (section.id === currentSection) {
-          linkElement.classList.add("active");
-        } else {
-          linkElement.classList.remove("active");
-        }
-      }
-    });
+    }
   });
+
+  sections.forEach((section) => {
+    const linkElement = document.getElementById(section.linkId);
+    if (linkElement) {
+      if (section.id === currentSection) {
+        linkElement.classList.add("active");
+      } else {
+        linkElement.classList.remove("active");
+      }
+    }
+  });
+
+  const heroSection = document.getElementById("hero");
+  const backToTopButton = document.getElementById("back-to-top");
+
+  const heroBottom = heroSection.getBoundingClientRect().top;
+
+  if (heroBottom < 0) {
+    backToTopButton.classList.remove("hidden");
+    backToTopButton.classList.add("visible");
+  } else {
+    backToTopButton.classList.remove("visible");
+    backToTopButton.classList.add("hidden");
+  }
+});
